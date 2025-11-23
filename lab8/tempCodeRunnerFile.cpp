f53103c4875a6fc0e6257552724e772200a35065
javeria0990 <f2025-0990@bnu.edu.pk>
2025-11-23 20:38:48 +0500

    lab1

---
+++ lab8/tempCodeRunnerFile.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using  namespace std;
+int main()
+{
+    int books;
+    int totalBooks;
+    int days;
+    int fine;
+    cout<<"enter number of books returned late"<<endl;
+    cin>>books;
+    while(totalBooks<=books)
+    {
+          b 
+      cout<<books<<endl;
+
+    }
+}
